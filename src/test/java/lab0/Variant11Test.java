e9b1b3e2afc832a2b0c86b0dee2a566969da37ee
trade <tradewcs@gmail.com>
2023-09-16 18:31:57 +0300

    add parameterized tests

--- src/test/java/lab0/Variant11Test.java
+++ src/test/java/lab0/Variant11Test.java
@@ -159,5 +159,4 @@ class Variant11Test {
     public Object[][] arrayProvider() {
         return new Object[][] {{new int[]{1, 2, 3, 4, 5}, 2, new int[]{2, 4}}, {new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 4, new int[]{4, 8}}};
     }
-
 }
